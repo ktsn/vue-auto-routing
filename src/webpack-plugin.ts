36973321a2fa7f81cbeb531f28650fa27de1dfb0
ZHAOLEITAO <819539651@qq.com>
2020-04-03 11:46:55 +0800

    support MPA router

--- src/webpack-plugin.ts
+++ src/webpack-plugin.ts
@@ -2,26 +2,43 @@ import * as assert from 'assert'
 import * as fs from 'fs'
 import * as path from 'path'
 import { Compiler } from 'webpack'
-import { generateRoutes, GenerateConfig } from 'vue-route-generator'
+import { generateRoutes } from 'vue-route-generator'
 
 const pluginName = 'VueAutoRoutingPlugin'
 
-interface Options extends GenerateConfig {}
+interface Options {
+  pages: string;
+  pageName?: string;
+  importPrefix?: string;
+  dynamicImport?: boolean;
+  chunkNamePrefix?: string;
+  nested?: boolean;
+}
 
 namespace VueAutoRoutingPlugin {
   export type AutoRoutingOptions = Options
 }
 
 class VueAutoRoutingPlugin {
-  constructor(private options: Options) {
-    assert(options.pages, '`pages` is required')
+  constructor(private options: Options | Options[]) {
+    if (Array.isArray(options)) {
+      options.forEach(option => {
+        assert(option.pages, '`pages` is required');
+      })
+    } else {
+      assert(options.pages, '`pages` is required');
+    }
   }
 
   apply(compiler: Compiler) {
-    const generate = () => {
-      const code = generateRoutes(this.options)
-      const to = path.resolve(__dirname, '../index.js')
-
+    const generate = (option: Options) => {
+      const code = generateRoutes(option)
+      let to;
+      if (option.pageName) {
+        to = path.resolve(__dirname, `../${option.pageName}.js`);
+      } else {
+        to = path.resolve(__dirname, '../index.js');
+      }
       if (
         fs.existsSync(to) &&
         fs.readFileSync(to, 'utf8').trim() === code.trim()
@@ -31,10 +48,33 @@ class VueAutoRoutingPlugin {
 
       fs.writeFileSync(to, code)
     }
+    const generateIndex = (options: Options[]) => {
+      let importCode = '', exportCode = '';
+      options.forEach(option => {
+        importCode += `import ${option.pageName} from './${option.pageName}';\n`
+        exportCode += `${option.pageName},`
+      })
+      return `${importCode}export{${exportCode}}`
+    }
 
     compiler.hooks.thisCompilation.tap(pluginName, compilation => {
       try {
-        generate()
+        if (Object.prototype.toString.call(this.options) === '[object Object]') {
+          generate(this.options as Options);
+        } else if (Array.isArray(this.options)) {
+          this.options.forEach(option => {
+            generate(option)
+          })
+          const indexPath = path.resolve(__dirname, '../index.js');
+          const code = generateIndex(this.options)
+          if (fs.existsSync(indexPath) &&
+            fs.readFileSync(indexPath, 'utf8').trim() === code.trim()) {
+            return;
+          }
+          fs.writeFileSync(indexPath, code);
+        } else {
+          throw new Error('options type need object or array')
+        }
       } catch (error) {
         compilation.errors.push(error)
       }
